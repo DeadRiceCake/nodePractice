bc0682e2d0cc8912b468bf9850bbdbca371bfa98
ggugg <gguggong@naver.com>
2021-12-01 18:39:17 +0900

    2021-12-01 공부

--- gif-chat/app.js
+++ gif-chat/app.js
@@ -5,25 +5,37 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const ColorHash = require('color-hash');
 
 dotenv.config();
 const webSocket = require('./socket');
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 
 const app = express();
 app.set('port', process.env.PORT || 8005);
 app.set('view engine', 'html');
-
 nunjucks.configure('views', {
     express: app,
     watch: true,
 });
+connect();
 
+const sessionMiddleWare = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+});
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleWare);
 app.use(session({
     resave: false,
     saveUninitialized: false,
@@ -33,6 +45,14 @@ app.use(session({
         secure: false,
     },
 }));
+
+app.use((req, res, next) => {
+    if(!req.session.color) {
+        const colorHash = new ColorHash();
+        req.session.color = colorHash.hex(req.sessionID);
+    }
+    next();
+});
 
 app.use('/', indexRouter);
 
@@ -53,4 +73,4 @@ const server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중...');
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleWare);
